056041b186a8b718f49832e6e54b2bf7b004ece8
katyjmt <43462706+katyjmt@users.noreply.github.com>
2023-09-04 10:45:42 +1000

    homepage updates, incl updates to handlebars files

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,3 +1,23 @@
 const router = require('express').Router();
+const Category = require('../models/category');
+const SubCategory = require('../models/subCategory');
+const Product = require('../models/product');
 
-module.exports = router;
+// Route to get all categories and render homepage with category data
+// router.get('/', async (req, res) => {
+//     try {
+//         const categoryTypes = await Category.findAll();
+//         const catData = category.map((catItem) =>
+//         catItem.get({ plain: true }));
+
+//         res.render('homepage', {layout: 'main'});
+//     } catch (err) {
+//         res.status(500).json(err);
+//     }
+// })
+
+router.get('/', (req, res) => {
+    res.render('homepage');
+});
+
+module.exports = router;    
