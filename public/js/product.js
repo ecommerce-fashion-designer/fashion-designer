ec4bcf0e777643e5a12f5b28642bf2e3502ec37c
lipika.manglaa <lipika.mangla.web@gmail.com>
2023-09-07 17:20:48 +1000

    Ui update

--- public/js/product.js
+++ public/js/product.js
@@ -15,11 +15,13 @@ productSearchItems.innerHTML="";
     filterResult.forEach(element => {
         productSearchItems.innerHTML+=` <div class="col-md-4">
         <div class="product-list-items">
-            <div class="card" style="width: 18rem;">
+            <div class="card" >
+            <div class="hover-img">
                 <a href="/product/${element.slug}">
                     <img class="card-img-top productImage" src='/${element.product_image_path}'
                         alt="Card image cap">
                 </a>
+                </div>
 
                 <div class="card-body">
                     <h5 class="card-title prodcut-title">${element.product_name}</h5>
@@ -39,8 +41,6 @@ productSearchItems.innerHTML="";
 
 
 
-
-//
 let priceFilter=document.querySelector("#price-filter")
 
 priceFilter.addEventListener("change",(e)=>{
@@ -62,11 +62,13 @@ console.log(element.product_image_path)
     if(Number(element.price)>=15 && Number(element.price)<filterValue)
     productSearchItems.innerHTML+=` <div class="col-md-4">
     <div class="product-list-items">
-        <div class="card" style="width: 18rem;">
+        <div class="card" ">
+        <div class="hover-img">
             <a href="/product/${element.slug}">
                 <img class="card-img-top productImage" src='/${element.product_image_path}'
                     alt="Card image cap">
             </a>
+            </div>
 
             <div class="card-body">
                 <h5 class="card-title prodcut-title">${element.product_name}</h5>
@@ -82,3 +84,50 @@ console.log(element.product_image_path)
     
 })
 })
+
+
+
+//
+// let priceFilter=document.querySelector("#price-filter")
+
+// priceFilter.addEventListener("change",(e)=>{
+//     productSearchItems.innerHTML="";
+//     fprice.innerHTML=e.target.value;
+//     console.log(e.target.value)
+//     let filterValue=Number(e.target.value)
+//     fetch('/api/products/price')
+//     .then((res)=>{
+//          return res.json()
+//     })
+//     .then((finalResponse)=>{
+//     return finalResponse.proData
+//  })
+//  .then((filterResult)=>{
+
+// filterResult.forEach(element => {
+// console.log(element.product_image_path)
+//     if(Number(element.price)>=15 && Number(element.price)<filterValue)
+//     productSearchItems.innerHTML+=` <div class="col-md-4">
+//     <div class="product-list-items">
+//         <div class="card" ">
+//         <div class="hover-img">
+//             <a href="/product/${element.slug}">
+//                 <img class="card-img-top productImage" src='/${element.product_image_path}'
+//                     alt="Card image cap">
+//             </a>
+//             </div>
+
+//             <div class="card-body">
+//                 <h5 class="card-title prodcut-title">${element.product_name}</h5>
+//                 <p class="card-text product-price">Price:${element.price}</p>
+
+//             </div>
+//         </div>
+
+//     </div>
+// </div>`;
+// });
+
+    
+// })
+// })
